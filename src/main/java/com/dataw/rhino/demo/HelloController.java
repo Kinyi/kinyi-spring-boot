223832cd9844d0f75157ff217a9f0e1d1e4317d3
kinyi <kinyi@hudongpai.com>
2018-11-26 15:30:48 +0800

    update add lombok log

--- src/main/java/com/dataw/rhino/demo/HelloController.java
+++ src/main/java/com/dataw/rhino/demo/HelloController.java
@@ -1,10 +1,12 @@
 package com.dataw.rhino.demo;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class HelloController {
     
@@ -15,6 +17,7 @@ public class HelloController {
 
     @RequestMapping(value = "/getName", method = RequestMethod.POST)
     public String getUserName(@RequestParam(value = "name") String userName) {
+        log.info("hi, " + userName);
         return "hi, " + userName;
     }
     
---
+++ src/test/java/com/dataw/rhino/demo/FirstApplicationTest.java
@@ -0,0 +1,11 @@
+package com.dataw.rhino.demo;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author Kinyi_Chan
+ * @since 2018-11-26
+ */
+public class FirstApplicationTest {
+
+}
